a4731e7e8bcaccec59f84158d60111f91d81cc22
Gabriel Hora <gabrielhoradeveloper@gmail.com>
2023-09-03 10:28:52 -0300

    cria função para resgatar topicos pelo id

--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -6,9 +6,9 @@ import br.com.alura.forum.model.Usuario
 import org.springframework.stereotype.Service
 
 @Service
-class TopicoService {
+class TopicoService(private var topicos: List<Topico>) {
 
-    fun listar(): List<Topico> {
+    init {
         val topico = Topico(
                 id = 1,
                 titulo = "Dúvida Kotlin",
@@ -25,6 +25,49 @@ class TopicoService {
                 )
         )
 
-        return listOf(topico, topico, topico)
+        val topico2 = Topico(
+                id = 2,
+                titulo = "Kotlin intermediário",
+                mensagem = "Variáveis do Kotlin",
+                curso = Curso(
+                        id = 1,
+                        nome = "Kotlin",
+                        categoria = "Programação"
+                ),
+                autor = Usuario(
+                        id = 1,
+                        nome = "Ana da Silva",
+                        email = "ana@email.com"
+                )
+        )
+
+        val topico3 = Topico(
+                id = 3,
+                titulo = "Kotlin avançado",
+                mensagem = "Variáveis do Kotlin",
+                curso = Curso(
+                        id = 1,
+                        nome = "Kotlin",
+                        categoria = "Programação"
+                ),
+                autor = Usuario(
+                        id = 1,
+                        nome = "Ana da Silva",
+                        email = "ana@email.com"
+                )
+        )
+
+        topicos = listOf(topico, topico2, topico3)
+    }
+
+    fun listar(): List<Topico> {
+        return topicos
+    }
+
+
+    fun buscarPorId(id: Long): Topico {
+        return topicos.stream().filter { t ->
+            t.id == id
+        }.findFirst().get()
     }
 }
