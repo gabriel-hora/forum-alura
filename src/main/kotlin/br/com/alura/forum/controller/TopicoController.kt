7b1001030da0ab7da834af7c8101604cf3cb1789
Gabriel Hora <gabrielhoradeveloper@gmail.com>
2023-09-03 10:14:37 -0300

    Implementa classe controller, recursos e service da api forum

---
+++ src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -0,0 +1,18 @@
+package br.com.alura.forum.controller
+
+import br.com.alura.forum.model.Topico
+import br.com.alura.forum.service.TopicoService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+
+@RestController
+@RequestMapping("/topicos")
+class TopicoController(private val service: TopicoService) {
+
+    @GetMapping
+    fun listar(): List<Topico> {
+        return service.listar()
+    }
+}
